f47558413aa331e9aa511fd91a7994bac0638807
Joshua <3729344@myuwc.ac.za>
2022-02-13 20:06:39 +0200

    changed the loadingSpinner component's css file to a css moe

--- src/Components/UI/LoadingSpinner/LoadingSpinner.js
+++ src/Components/UI/LoadingSpinner/LoadingSpinner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./LoadingSpinner.css";
+import classes from "./LoadingSpinner.module.css";
 
 const LoadingSpinner = () => {
   return <div className={classes.loader}>Loading...</div>;
