8d8148ab87ba0e1167fdbeec684c1d1a80e55a7e
Joshua <3729344@myuwc.ac.za>
2022-02-13 19:40:20 +0200

    added a amax component component

---
+++ src/Components/Amax/Amax Card/AmaxCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./AmaxCard.css";
+import Button from "../../UI/Button/Button";
+
+const amaxCard = () => {
+  return (
+    <div className={classes.AmaxCard}>
+      <div className={classes.Wrapper}>
+        <h1 className={classes.Header1}>EXPERIENCE MORE</h1>
+        <h1 className={classes.Header2}>WITH M-DAB EVENTS</h1>
+        <Button label="CREATE ACCOUNT" />
+      </div>
+    </div>
+  );
+};
+
+export default amaxCard;
---
+++ src/Components/Amax/Amax.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Amax.css";
+import AmaxCard from "./Amax Card/AmaxCard";
+
+const amax = () => {
+  return (
+    <section className={classes.Amax}>
+      <AmaxCard />
+    </section>
+  );
+};
+
+export default amax;
