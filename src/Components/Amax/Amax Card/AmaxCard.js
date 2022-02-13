a8fa01c37d7dd9c8b04ca2824bb3fe9e879a14b7
Joshua <3729344@myuwc.ac.za>
2022-02-13 19:41:39 +0200

    changed amaxCard css file

--- src/Components/Amax/Amax Card/AmaxCard.js
+++ src/Components/Amax/Amax Card/AmaxCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./AmaxCard.css";
+import classes from "./AmaxCard.module.css";
 import Button from "../../UI/Button/Button";
 
 const amaxCard = () => {
