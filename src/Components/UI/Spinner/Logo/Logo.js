acb34ae39799c3a3b43554d655b5d1a7b35748cf
Joshua <3729344@myuwc.ac.za>
2022-02-13 20:09:26 +0200

    changed the Spinner component's css file to a css module

--- src/Components/UI/Spinner/Logo/Logo.js
+++ src/Components/UI/Spinner/Logo/Logo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./Logo.css";
+import classes from "./Logo.module.css";
 import Logo from "../../../../Assets/images/Logo1.png";
 const logo = () => {
   return (
