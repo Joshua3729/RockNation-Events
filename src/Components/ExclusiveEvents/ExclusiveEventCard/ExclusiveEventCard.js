3d4ae6be5339890f28668cd1464ea65b96135063
Joshua <3729344@myuwc.ac.za>
2022-02-16 15:09:58 +0200

    added ExclusiveEvents component

---
+++ src/Components/ExclusiveEvents/ExclusiveEventCard/ExclusiveEventCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./ExclusiveEventCard.css";
+import SolidButton from "../../UI/SolidButton/SolidButton";
+
+const exclusiveEventCard = (props) => {
+  return (
+    <div className={classes.ExclusiveEventCard}>
+      <div className={classes.ImageWrapper}>
+        <img src={props.img} alt="" />
+      </div>
+      <div className={classes.TextWrapper}>
+        <p className={classes.Description}>{props.description}</p>
+        <h2 className={classes.SecondaryHeader}>{props.header}</h2>
+        <p className={classes.Text}>{props.text}</p>
+        <div className={classes.ButtonWrapper}>
+          <SolidButton>{props.value}</SolidButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default exclusiveEventCard;
