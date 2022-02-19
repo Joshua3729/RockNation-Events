e0813477016b95ff20e96da0d0d5aa282709f0bd
Joshua <3729344@myuwc.ac.za>
2022-02-20 01:30:56 +0200

    changed css file to a css module

--- src/Components/Event/Event.js
+++ src/Components/Event/Event.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import classes from "./Event.css";
+import classes from "./Event.module.css";
 
 const event = (props) => {
   return (
