40d794a672e0940769abf6f75b37060c07ac4286
Joshua <3729344@myuwc.ac.za>
2022-02-20 23:33:55 +0200

    working on the heroSection

--- src/Components/HeroSection/Hero.js
+++ src/Components/HeroSection/Hero.js
@@ -3,21 +3,23 @@ import classes from "./Hero.module.css";
 import video from "../../Assets/Video/bg.mp4";
 import { Link } from "react-router-dom";
 import Button from "../UI/Button/Button";
+import hero from "../Image/hero.png";
 const Hero = () => {
   return (
     <div className={classes.Hero}>
       <div className={classes.backDrop}></div>
       <video src={video} autoPlay loop muted />
       <div className={classes.Wrapper}>
-        <h1 className={classes.primary_header}>LET'S GET THE PARTY STARTED!</h1>
+        {/* <h1 className={classes.primary_header}>LET'S GET THE PARTY STARTED!</h1>
         <p className={classes.Hero_description}>
           Shop for millions of tickets for music events from your favourite
           artist
         </p>
         <Link to="/eventmanager">
           <Button label="GET STARTED" />
-        </Link>
+        </Link> */}
       </div>
+      <img src={hero} alt="" className={classes.heroImg} />
     </div>
   );
 };
