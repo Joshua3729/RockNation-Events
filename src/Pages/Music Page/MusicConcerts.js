13ce4dbfcf86212a46d639278296853fd0fe2a67
Joshua <3729344@myuwc.ac.za>
2022-02-24 05:09:32 +0200

    working on the music concerts page

--- src/Pages/Music Page/MusicConcerts.js
+++ src/Pages/Music Page/MusicConcerts.js
@@ -30,6 +30,29 @@ class MusicConcerts extends Component {
           </div>
           <div className={classes.mainContent}>
             <h2 className={classes.header}>All Concert Events (783)</h2>
+            <div className={classes.events}>
+              <div className={classes.event}>
+                <div className={classes.imgWrapper}>
+                  <img
+                    src="https://www.aceshowbiz.com/images/photo/coldplay.jpg"
+                    alt=""
+                    className={classes.eventImg}
+                  />
+                </div>
+
+                <div className={classes.dateWrapper}>
+                  <p className={classes.date}>FEB 10</p>
+                  <div className={classes.time}>Thu . 6:30PM</div>
+                </div>
+                <div className={classes.vanueWrapper}>
+                  <p className={classes.eventName}>
+                    Andres, Colleta, I met a Yeti
+                  </p>
+                  <p className={classes.venue}>Staple Center, New York, NY</p>
+                </div>
+                <button className={classes.seeTickets}>See Tickets</button>
+              </div>
+            </div>
           </div>
         </section>
       </Fragment>
