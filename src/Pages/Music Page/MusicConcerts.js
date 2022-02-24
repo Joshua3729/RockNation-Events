630db4fe54d1fe0d943db431b9def2d54d066496
Joshua <3729344@myuwc.ac.za>
2022-02-24 05:44:58 +0200

    working on the music concerts page

--- src/Pages/Music Page/MusicConcerts.js
+++ src/Pages/Music Page/MusicConcerts.js
@@ -13,6 +13,31 @@ class MusicConcerts extends Component {
   };
 
   render() {
+    let events = [1, 2, 3, 4, 5, 6, 7, 8].map((event, i) => {
+      return (
+        <div className={classes.event} key={i}>
+          <div className={classes.imgWrapper}>
+            <img
+              src="https://www.aceshowbiz.com/images/photo/coldplay.jpg"
+              alt=""
+              className={classes.eventImg}
+            />
+          </div>
+
+          <div className={classes.dateWrapper}>
+            <p className={classes.date}>FEB 10</p>
+            <div className={classes.time}>Thu . 6:30PM</div>
+          </div>
+          <div className={classes.vanueWrapper}>
+            <p className={classes.eventName}>Andres, Colleta, I met a Yeti</p>
+            <p className={classes.venue}>Staple Center, New York, NY</p>
+          </div>
+          <div className={classes.btnWrapper}>
+            <button className={classes.seeTickets}>See Tickets</button>
+          </div>
+        </div>
+      );
+    });
     return (
       <Fragment>
         <Navigation
@@ -30,29 +55,7 @@ class MusicConcerts extends Component {
           </div>
           <div className={classes.mainContent}>
             <h2 className={classes.header}>All Concert Events (783)</h2>
-            <div className={classes.events}>
-              <div className={classes.event}>
-                <div className={classes.imgWrapper}>
-                  <img
-                    src="https://www.aceshowbiz.com/images/photo/coldplay.jpg"
-                    alt=""
-                    className={classes.eventImg}
-                  />
-                </div>
-
-                <div className={classes.dateWrapper}>
-                  <p className={classes.date}>FEB 10</p>
-                  <div className={classes.time}>Thu . 6:30PM</div>
-                </div>
-                <div className={classes.vanueWrapper}>
-                  <p className={classes.eventName}>
-                    Andres, Colleta, I met a Yeti
-                  </p>
-                  <p className={classes.venue}>Staple Center, New York, NY</p>
-                </div>
-                <button className={classes.seeTickets}>See Tickets</button>
-              </div>
-            </div>
+            <div className={classes.events}>{events}</div>
           </div>
         </section>
       </Fragment>
