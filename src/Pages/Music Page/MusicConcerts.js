af8cce354721a82efe36286ae223137aeb47d23a
Joshua <3729344@myuwc.ac.za>
2022-02-26 04:34:39 +0200

    added ad video

--- src/Pages/Music Page/MusicConcerts.js
+++ src/Pages/Music Page/MusicConcerts.js
@@ -85,6 +85,7 @@ class MusicConcerts extends Component {
               <div className={classes.events}>{events}</div>
               <div className={classes.AdCard}>
                 <img src="https://images.unsplash.com/photo-1541126274323-dbac58d14741?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
+                <div className={classes.backDrop}></div>
               </div>
             </div>
           </div>
