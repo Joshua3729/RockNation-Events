4649b584018c4a6bdd5733d4fb00fe012fe5063b
Joshua <3729344@myuwc.ac.za>
2022-02-28 09:25:16 +0200

    working on comingsoon

--- src/Components/Coming Soon/ComingSoon.js
+++ src/Components/Coming Soon/ComingSoon.js
@@ -11,10 +11,38 @@ const ComingSoon = (props) => {
     <section className={classes.upcomingEvents}>
       <h2 className={classes.header}>Our top picks this week</h2>
       <div className={classes.OurPicksGrid}>
-        <EventCard img={beiber} />
-        <EventCard img={badbunny} />
-        <EventCard img={billie} />
-        <EventCard img={khalid} />
+        <EventCard
+          event={{
+            cardImg: beiber,
+            eventName: "Justin Beiber: Turn On The Lights",
+            venue: "The O2",
+            city: "Manchester",
+          }}
+        />
+        <EventCard
+          event={{
+            cardImg: badbunny,
+            eventName: "Bad Bunny: Game on",
+            venue: "staple center",
+            city: "New York",
+          }}
+        />
+        <EventCard
+          event={{
+            cardImg: billie,
+            eventName: "Billy Eilish: Simphony tour",
+            venue: "Rumble Arena",
+            city: "Australia",
+          }}
+        />
+        <EventCard
+          event={{
+            cardImg: khalid,
+            eventName: "Khalid: Bad Boys Tour",
+            venue: "Wembly Stadium",
+            city: "London",
+          }}
+        />
       </div>
     </section>
   );
