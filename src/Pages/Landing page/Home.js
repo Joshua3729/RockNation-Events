e7e641516bb95a29a78ced8c27bea1538f8b8170
Joshua <3729344@myuwc.ac.za>
2022-03-07 05:44:51 +0200

    removed unused code

--- src/Pages/Landing page/Home.js
+++ src/Pages/Landing page/Home.js
@@ -7,109 +7,34 @@ import Amax from "../../Components/Amax/Amax";
 import ExclusiveEvents from "../../Components/ExclusiveEvents/ExclusiveEvents";
 import Navigation from "../../Components/Navigation/Navigation";
 import Categories from "../../Components/Categories/Categories";
-import LoadingModal from "../../Components/Loading Modal/LoadingModal";
 import { Fragment } from "react/cjs/react.development";
-import classes from "./Home.module.css";
-// import LoadingModal from "../../Components/Loading Modal/LoadingModal";
 
 class Home extends Component {
-  state = {
-    concerts: null,
-    sports: null,
-    artsandtheater: null,
-    concertsLoading: true,
-    sportsLoading: true,
-    artsAndTheaterLoading: true,
-  };
-
-  getConcerts = () => {
-    fetch("http://localhost:5000/feed/events/concerts")
-      .then((res) => {
-        if (res.status !== 200) {
-          throw new Error("Failed to fetch concerts.");
-        }
-
-        return res.json();
-      })
-      .then((resData) => {
-        this.setState({
-          concerts: resData.events,
-          // concertsLoading: false,
-        });
-      })
-      .catch((err) => {
-        this.setState({ concertsLoading: false });
-        console.log(err);
-      });
-  };
-  getSports = () => {
-    fetch("http://localhost:5000/feed/events/sports")
-      .then((res) => {
-        if (res.status !== 200) {
-          throw new Error("Failed to fetch events.");
-        }
-
-        return res.json();
-      })
-      .then((resData) => {
-        this.setState({
-          sports: resData.events,
-          sportsLoading: false,
-        });
-      })
-      .catch((err) => {
-        this.setState({ sportsLoading: false });
-        console.log(err);
-      });
-  };
-  getArtsAndTheater = () => {
-    fetch("http://localhost:5000/feed/events/artsandtheater")
-      .then((res) => {
-        if (res.status !== 200) {
-          throw new Error("Failed to fetch events.");
-        }
-
-        return res.json();
-      })
-      .then((resData) => {
-        this.setState({
-          artsandtheater: resData.events,
-          artsAndTheaterLoading: false,
-        });
-      })
-      .catch((err) => {
-        this.setState({ artsAndTheaterLoading: false });
-        console.log(err);
-      });
-  };
   render() {
     return (
       <Fragment>
-        {loading && <LoadingModal />}
-        <div className={loading ? classes.loading : null}>
-          <Navigation
-            scrollEffect={true}
-            searchBar={false}
-            isAuth={this.props.isAuth}
-            logout={this.props.logout}
-            login={this.props.loginModal}
-            fullname={this.props.fullname}
-            userImage={this.props.userImage}
-          />
-          <Hero />
-          <ComingSoon />
-          <Categories />
-          <TopSelling
-            concerts={this.state.concerts}
-            getConcerts={this.getConcerts}
-            sports={this.state.sports}
-            getSports={this.getSports}
-            artsandtheater={this.state.artsandtheater}
-            getArtsAndTheater={this.getArtsAndTheater}
-          />
-          <Amax />
-          <ExclusiveEvents />)
-        </div>
+        <Navigation
+          scrollEffect={true}
+          searchBar={false}
+          isAuth={this.props.isAuth}
+          logout={this.props.logout}
+          login={this.props.loginModal}
+          fullname={this.props.fullname}
+          userImage={this.props.userImage}
+        />
+        <Hero />
+        <ComingSoon />
+        <Categories />
+        <TopSelling
+          concerts={this.state.concerts}
+          getConcerts={this.getConcerts}
+          sports={this.state.sports}
+          getSports={this.getSports}
+          artsandtheater={this.state.artsandtheater}
+          getArtsAndTheater={this.getArtsAndTheater}
+        />
+        <Amax />
+        <ExclusiveEvents />)
       </Fragment>
     );
   }
