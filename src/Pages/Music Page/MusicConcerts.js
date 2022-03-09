14c748662032d0f6c85881ab82d593c050693fb1
Joshua <3729344@myuwc.ac.za>
2022-03-09 23:53:35 +0200

    removed adCard div

--- src/Pages/Music Page/MusicConcerts.js
+++ src/Pages/Music Page/MusicConcerts.js
@@ -80,9 +80,6 @@ class MusicConcerts extends Component {
             <h2 className={classes.header}>All Concert Events (783)</h2>
             <div className={classes.eventsWrapper}>
               <div className={classes.events}>{events}</div>
-              <div className={classes.AdCard}>
-                <video src={video} autoPlay loop muted />
-              </div>
             </div>
           </div>
         </section>
