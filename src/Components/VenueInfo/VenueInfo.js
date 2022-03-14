4cceaab8f6d81f1b6d6a94610329f7ff9c4a699b
Joshua <3729344@myuwc.ac.za>
2022-03-14 03:34:45 +0200

    added to venueInfo

---
+++ src/Components/VenueInfo/VenueInfo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./ArtistInfo.module.css";
+
+const venueInfo = (props) => {
+  return (
+    <div className={classes.event}>
+      <div className={classes.imgWrapper}>
+        <img src={props.event.img} alt="" className={classes.eventImg} />
+      </div>
+      <div className={classes.aboutArtistWrapper}>
+        <p className={classes.name}>{props.event.name}</p>
+        <p className={classes.location}>{props.event.genre}</p>
+      </div>
+    </div>
+  );
+};
+
+export default venueInfo;
