c87f31e88878c58706b96214e570429a9cacab79
Joshua <3729344@myuwc.ac.za>
2022-03-15 03:30:54 +0200

    added to view events page

---
+++ src/Pages/viewEvents Page/ViewEvents.js
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from "react";
+import classes from "./SearchResults.module.css";
+import Navigation from "../../Components/Navigation/Navigation";
+import EventInfo from "../../Components/EventInfo/EventInfo";
+import { withRouter } from "react-router-dom";
+
+class ViewEvents extends Component {
+  state = {};
+
+  componentDidMount = () => {};
+  render() {
+    return (
+      <Fragment>
+        <Navigation
+          scrollEffect={true}
+          searchBar={true}
+          isAuth={this.props.isAuth}
+          logout={this.props.logout}
+          login={this.props.loginModal}
+          fullname={this.props.fullname}
+          userImage={this.props.userImage}
+          home={this.props.goToHome}
+          search={this.props.search}
+        />
+        <section className={classes.ViewEvents}>
+          <div className={classes.banner}></div>
+          <div className={classes.mainContent}></div>
+        </section>
+      </Fragment>
+    );
+  }
+}
+
+export default withRouter(ViewEvents);
