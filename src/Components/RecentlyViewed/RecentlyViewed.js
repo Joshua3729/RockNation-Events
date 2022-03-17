c2e5c9630085c63caff88243a26951ca47127d39
Joshua <3729344@myuwc.ac.za>
2022-03-17 05:46:44 +0200

    added re

---
+++ src/Components/RecentlyViewed/RecentlyViewed.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./RecentlyViewed.module.css";
+
+const recentlyViewed = (props) => {
+  return (
+    <div className={classes.recentlyViewed}>
+      <div className={classes.recentlyViewed_item}></div>
+      <div className={classes.recentlyViewed_item}></div>
+      <div className={classes.recentlyViewed_item}></div>
+      <div className={classes.recentlyViewed_item}></div>
+    </div>
+  );
+};
+
+export default recentlyViewedData;
