573028942f9e77705abe61df64c802219f45d0bc
Joshua <3729344@myuwc.ac.za>
2022-03-17 05:53:04 +0200

    added to recently viewed component

--- src/Components/RecentlyViewed/RecentlyViewed.js
+++ src/Components/RecentlyViewed/RecentlyViewed.js
@@ -3,11 +3,14 @@ import classes from "./RecentlyViewed.module.css";
 
 const recentlyViewed = (props) => {
   return (
-    <div className={classes.recentlyViewed}>
-      <div className={classes.recentlyViewed_item}></div>
-      <div className={classes.recentlyViewed_item}></div>
-      <div className={classes.recentlyViewed_item}></div>
-      <div className={classes.recentlyViewed_item}></div>
+    <div className={classes.recentlyViewed_wrapper}>
+      <div className={classes.sectionDescription}>Recently Viewed</div>
+      <div className={classes.recentlyViewed}>
+        <div className={classes.recentlyViewed_item}></div>
+        <div className={classes.recentlyViewed_item}></div>
+        <div className={classes.recentlyViewed_item}></div>
+        <div className={classes.recentlyViewed_item}></div>
+      </div>
     </div>
   );
 };
