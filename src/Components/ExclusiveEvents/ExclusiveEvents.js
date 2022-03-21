a1e542952dcfe39a7c51ffeffb4a447277efbf1d
Joshua <3729344@myuwc.ac.za>
2022-03-21 10:16:11 +0200

    made changes to the exclusive events sections

--- src/Components/ExclusiveEvents/ExclusiveEvents.js
+++ src/Components/ExclusiveEvents/ExclusiveEvents.js
@@ -4,24 +4,26 @@ import ExclusiveEventCard from "./ExclusiveEventCard/ExclusiveEventCard";
 const exclusiveEvents = () => {
   return (
     <section className={classes.ExclusiveEvents}>
-      <div className={classes.GridItem}>
-        <ExclusiveEventCard
-          text="With access to special events and experiences from Resy, get a taste
+      <div className={classes.ExclusiveEvents_wrapper}>
+        <div className={classes.GridItem}>
+          <ExclusiveEventCard
+            text="With access to special events and experiences from Resy, get a taste
           for a whole new way to dine."
-          description="DINING"
-          header="Dining with Resy"
-          value="Explore Dining"
-          img="https://thepointsguy.global.ssl.fastly.net/us/originals/2021/07/Resy-GDA-02.jpg"
-        />
-      </div>
-      <div className={classes.GridItem}>
-        <ExclusiveEventCard
-          value="Discover Experiences"
-          description="BY INVITATION ONLY®"
-          header="Elevated Experiences"
-          img="https://www.washingtonian.com/wp-content/uploads/2016/02/extravagant-wedding-2.jpg"
-          text="By Invitation Only puts the extraordinary within reach. Platinum Card® and Centurion® Card Members can purchase tickets to customized experiences in sports, fashion, fine dining, arts and theater."
-        />
+            description="DINING"
+            header="Dining with Resy"
+            value="Explore Dining"
+            img="https://thepointsguy.global.ssl.fastly.net/us/originals/2021/07/Resy-GDA-02.jpg"
+          />
+        </div>
+        <div className={classes.GridItem}>
+          <ExclusiveEventCard
+            value="Discover Experiences"
+            description="BY INVITATION ONLY®"
+            header="Elevated Experiences"
+            img="https://www.washingtonian.com/wp-content/uploads/2016/02/extravagant-wedding-2.jpg"
+            text="By Invitation Only puts the extraordinary within reach. Platinum Card® and Centurion® Card Members can purchase tickets to customized experiences in sports, fashion, fine dining, arts and theater."
+          />
+        </div>
       </div>
     </section>
   );
