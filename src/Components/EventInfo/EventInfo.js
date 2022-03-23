0f0df978206568858608f585bcf478c62a690853
Joshua <3729344@myuwc.ac.za>
2022-03-23 20:32:41 +0200

    added to the search suggestion component

--- src/Components/EventInfo/EventInfo.js
+++ src/Components/EventInfo/EventInfo.js
@@ -2,27 +2,67 @@ import React from "react";
 import classes from "./EventInfo.module.css";
 
 const eventInfo = (props) => {
-  return (
-    <div className={props.hideImage ? classes.hideImgEvents : classes.event}>
-      {props.hideImage ? null : (
+  let eventInfo = null;
+  if (props.searchSuggestion) {
+    eventInfo = (
+      <div className={classes.event}>
         <div className={classes.imgWrapper}>
-          <img src={props.event.eventImg} alt="" className={classes.eventImg} />
+          <img
+            src={props.event.img}
+            alt=""
+            className={classes.eventImg}
+            style={{
+              width: props.searchSuggestion && "60px",
+              borderRadius: props.searchSuggestion && "50%",
+            }}
+          />
         </div>
-      )}
+        <div className={classes.aboutArtistWrapper}>
+          <div
+            className={classes.name}
+            onClick={props.viewEntity.bind(this, props.event, "artist")}
+            style={{ fontSize: props.searchSuggestion && "15px" }}
+          >
+            {props.event.name}
+          </div>
 
-      <div className={classes.dateWrapper}>
-        <p className={classes.date}>FEB 10</p>
-        <div className={classes.time}>Thu . 6:30PM</div>
+          <p
+            className={classes.genre}
+            style={{ fontSize: props.searchSuggestion && "12px" }}
+          >
+            {props.event.genre}
+          </p>
+        </div>
       </div>
-      <div className={classes.vanueWrapper}>
-        <p className={classes.eventName}>{props.event.eventName}</p>
-        <p className={classes.venue}>{props.event.venue}</p>
+    );
+  } else {
+    eventInfo = (
+      <div className={props.hideImage ? classes.hideImgEvents : classes.event}>
+        {props.hideImage ? null : (
+          <div className={classes.imgWrapper}>
+            <img
+              src={props.event.eventImg}
+              alt=""
+              className={classes.eventImg}
+            />
+          </div>
+        )}
+
+        <div className={classes.dateWrapper}>
+          <p className={classes.date}>FEB 10</p>
+          <div className={classes.time}>Thu . 6:30PM</div>
+        </div>
+        <div className={classes.vanueWrapper}>
+          <p className={classes.eventName}>{props.event.eventName}</p>
+          <p className={classes.venue}>{props.event.venue}</p>
+        </div>
+        <div className={classes.btnWrapper}>
+          <button className={classes.seeTickets}>See Tickets</button>
+        </div>
       </div>
-      <div className={classes.btnWrapper}>
-        <button className={classes.seeTickets}>See Tickets</button>
-      </div>
-    </div>
-  );
+    );
+  }
+  return eventInfo;
 };
 
 export default eventInfo;
