7513fa774f28894997059482534fdba86bcc2ccf
Joshua <3729344@myuwc.ac.za>
2022-03-25 19:13:23 +0200

    added to See Tickets page

---
+++ src/Pages/See Tickets Page/SeeTickets.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classes from "./SeeTickets.module.css";
+
+class SeeTickets extends Component {
+  render() {
+    return <div className={classes.SeeTickets}></div>;
+  }
+}
+
+export default SeeTickets;
