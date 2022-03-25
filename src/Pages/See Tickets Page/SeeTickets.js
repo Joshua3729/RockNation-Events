d63fe06ca67f2f16c20a4909b2e198be62ab8082
Joshua <3729344@myuwc.ac.za>
2022-03-25 20:27:11 +0200

    added nav ba

--- src/Pages/See Tickets Page/SeeTickets.js
+++ src/Pages/See Tickets Page/SeeTickets.js
@@ -1,10 +1,22 @@
 import React, { Component, Fragment } from "react";
 import classes from "./SeeTickets.module.css";
+import Navigation from "../../Components/Navigation/Navigation";
 
 class SeeTickets extends Component {
   render() {
     return (
       <Fragment>
+        <Navigation
+          scrollEffect={true}
+          searchBar={true}
+          isAuth={this.props.isAuth}
+          logout={this.props.logout}
+          login={this.props.loginModal}
+          fullname={this.props.fullname}
+          userImage={this.props.userImage}
+          home={this.props.goToHome}
+          search={this.props.search}
+        />
         <div className={classes.SeeTickets}></div>
       </Fragment>
     );
