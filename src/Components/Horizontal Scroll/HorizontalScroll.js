7609fd9892b91bc2e7b0b545f4276a1849d1740d
Joshua <3729344@myuwc.ac.za>
2022-03-27 13:59:05 +0200

    added to  horizontal scroll component

--- src/Components/Horizontal Scroll/HorizontalScroll.js
+++ src/Components/Horizontal Scroll/HorizontalScroll.js
@@ -1,13 +1,54 @@
 import React from "react";
 import classes from "./HorizontalScroll.module.css";
+import EventCard from "../EventCard/EventCard";
+import beiber from "../Image/top_picks/beiber.jpg";
+import badbunny from "../Image/top_picks/badbunny.jpg";
+import billie from "../Image/top_picks/billie.jpg";
+import khalid from "../Image/top_picks/khalid.jpg";
 
 const scrollingHorizontally = (props) => {
   return (
     <div className={classes.parent}>
-      <div className={classes.scroll_item}>slide 1</div>
-      <div className={classes.scroll_item}>slide 2</div>
-      <div className={classes.scroll_item}>slide 3</div>
-      <div className={classes.scroll_item}>slide 4</div>
+      <div className={classes.scroll_card}>
+        <EventCard
+          event={{
+            cardImg: beiber,
+            eventName: "Justin Beiber: Turn On The Lights",
+            venue: "The O2",
+            city: "Manchester",
+          }}
+        />
+      </div>
+      <div className={classes.scroll_card}>
+        <EventCard
+          event={{
+            cardImg: badbunny,
+            eventName: "Bad Bunny: Game on",
+            venue: "staple center",
+            city: "New York",
+          }}
+        />
+      </div>
+      <div className={classes.scroll_card}>
+        <EventCard
+          event={{
+            cardImg: badbunny,
+            eventName: "Bad Bunny: Game on",
+            venue: "staple center",
+            city: "New York",
+          }}
+        />
+      </div>
+      <div className={classes.scroll_card}>
+        <EventCard
+          event={{
+            cardImg: khalid,
+            eventName: "Khalid: Bad Boys Tour",
+            venue: "Wembly Stadium",
+            city: "London",
+          }}
+        />
+      </div>
     </div>
   );
 };
