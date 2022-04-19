29c39c08d3656ab528dbe0d6c88b2a469026535c
Joshua <3729344@myuwc.ac.za>
2022-04-19 21:39:35 +0200

    added goToLink function

---
+++ src/Util/goToLink.js
@@ -0,0 +1,13 @@
+export const goToLink = (
+  eventName,
+  id,
+  type,
+  venueName,
+  artistName,
+  event_type
+) => {
+  this.props.history.push({
+    pathname: `/tickets/${eventName}/${id}`,
+    search: `?${type}=${artistName}&venue=${venueName}&event_type=${event_type}`,
+  });
+};
