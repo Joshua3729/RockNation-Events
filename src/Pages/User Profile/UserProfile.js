55f96a3338aff690104aab3f4cf7bcfeb8026028
Joshua <3729344@myuwc.ac.za>
2022-05-06 21:18:10 +0200

    added NavBar

--- src/Pages/User Profile/UserProfile.js
+++ src/Pages/User Profile/UserProfile.js
@@ -6,6 +6,17 @@ class UserProfile extends Component {
   render() {
     return (
       <Fragment>
+        <Navigation
+          scrollEffect={false}
+          searchBar={true}
+          isAuth={this.props.isAuth}
+          logout={this.props.logout}
+          login={this.props.loginModal}
+          fullname={this.props.fullname}
+          userImage={this.props.userImage}
+          home={this.props.goToHome}
+          search={this.props.search}
+        />
         <div className={classes.UserProfile}>
           <h1>User Profile</h1>
         </div>
