8bd3c868b106af17780910ae0c86790d26f43a3b
Joshua <3729344@myuwc.ac.za>
2022-05-07 02:14:29 +0200

    added to user profile page

--- src/Pages/User Profile/UserProfile.js
+++ src/Pages/User Profile/UserProfile.js
@@ -19,9 +19,23 @@ class UserProfile extends Component {
           search={this.props.search}
         />
         <div className={classes.UserProfile}>
-          <div className={classes.userDetails_wrapper}>
-            <img className={classes.profile_img} src={this.props.userImage} />
-            <h4 className={classes.username}>{this.props.fullname}</h4>
+          <div className={classes.innerWrapper}>
+            <div className={classes.userDetails_wrapper}>
+              <img
+                className={classes.profile_img}
+                src={"http://localhost:5000/" + this.props.userImage}
+              />
+              <div className={classes.userDetails_item_wrapper}>
+                <p className={classes.username}>{this.props.fullname}</p>
+                <div className={classes.stats_wrapper}>
+                  <p className={classes.stats_items}>0 orders </p>
+                  <span className={classes.seperator}>•</span>
+                  <p className={classes.stats_items}>0 likes</p>
+                  <span className={classes.seperator}>•</span>
+                  <p className={classes.stats_items}>0 follows</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <Footer />
