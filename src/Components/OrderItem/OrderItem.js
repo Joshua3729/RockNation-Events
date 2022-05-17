a068a4806a7f729042d35fd22be32d948496b17a
Joshua <3729344@myuwc.ac.za>
2022-05-17 20:29:40 +0200

    added to order item component

---
+++ src/Components/OrderItem/OrderItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./OrderItem.module.css";
+
+const orderItem = (props) => {
+  const order_item = props.orderItem;
+  return (
+    <div className={classes.OrderItem}>
+      <div className={classes.order_subItem}>{order_item._id}</div>
+      <div className={classes.order_subItem}>12 June 2022</div>
+      <div className={classes.order_subItem}>{order_item.event.name}</div>
+      <div className={classes.order_subItem}>{order_item.delivered}</div>
+      <div className={classes.order_subItem}>{order_item.price}</div>
+      <div className={classes.order_subItem}>Paid</div>
+    </div>
+  );
+};
+
+export default orderItem;
