7ede67abb66d4ad23652285a63dbb71a04b1b6ff
Joshua <3729344@myuwc.ac.za>
2022-05-19 19:51:25 +0200

    added to order item component

--- src/Components/OrderItem/OrderItem.js
+++ src/Components/OrderItem/OrderItem.js
@@ -8,7 +8,13 @@ const orderItem = (props) => {
       <div className={classes.order_subItem}>{order_item._id}</div>
       <div className={classes.order_subItem}>12 June 2022</div>
       <div className={classes.order_subItem}>{order_item.event.name}</div>
-      <div className={classes.order_subItem}>{`${order_item.delivered}`}</div>
+      <div className={classes.order_subItem}>
+        {order_item.delivered ? (
+          <p className={classes.fulfillied}>Fulfillied</p>
+        ) : (
+          <p className={classes.pending}>Pending Receipt</p>
+        )}
+      </div>
       <div className={classes.order_subItem}>${order_item.totalPrice}</div>
       <div className={classes.order_subItem}>Paid</div>
     </div>
